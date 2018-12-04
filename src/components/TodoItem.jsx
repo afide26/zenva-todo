e7e31c884bd161952bf793a2a52d58dde75fa522
Alan Fidelino <afidedev26@gmail.com>
2018-12-04 20:40:31 +1100

    Set up state change for checkbox

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
 import { Table, Checkbox, Button } from 'semantic-ui-react';
 
-const styles = {
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-  letterSpacing: "1px",
-  marginBottom: "10px"
-}
-const TodoItem = ({ children }) => {
+const TodoItem = ({ children, completed, onChange }) => {
   return (
-    <Table.Row>
+    <Table.Row positive={completed}>
       <Table.Cell>
-        <Checkbox />
+        <Checkbox onChange={onChange} />
       </Table.Cell>
       <Table.Cell>
         {children}
-        <Button color="red" icon="trash" floated="right" compact size="small" />
+        <Button color="red" icon="trash" floated="right" compact size="small" positive={completed} />
       </Table.Cell>
     </Table.Row>
   );
